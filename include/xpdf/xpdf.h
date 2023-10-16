d23740ec64b7e5f9adc1f9dca82ff03d99117566
Thorsten Otto <admin@tho-otto.de>
2023-10-16 11:04:32 +0200

    Add master include file xpdf.h for simpler use

---
+++ include/xpdf/xpdf.h
@@ -0,0 +1,46 @@
+/*
+ *========================================================================
+ *
+ * xpdf.h
+ *
+ * Copyright 1996-2022 Glyph & Cog, LLC
+ *
+ *========================================================================
+ */
+
+#ifndef XPDF_XPDF_H
+#define XPDF_XPDF_H
+
+#include <xpdf/aconf.h>
+#include <xpdf/xpdf/config.h>
+
+#include <xpdf/goo/GString.h>
+
+#include <xpdf/splash/Splash.h>
+#include <xpdf/splash/SplashBitmap.h>
+
+#include <xpdf/xpdf/GlobalParams.h>
+#include <xpdf/xpdf/Object.h>
+#include <xpdf/xpdf/PDFDoc.h>
+#include <xpdf/xpdf/SplashOutputDev.h>
+#include <xpdf/xpdf/Error.h>
+#include <xpdf/xpdf/AcroForm.h>
+#include <xpdf/xpdf/Annot.h>
+#include <xpdf/xpdf/Array.h>
+#include <xpdf/xpdf/Catalog.h>
+#include <xpdf/xpdf/CharTypes.h>
+#include <xpdf/xpdf/Dict.h>
+#include <xpdf/xpdf/Function.h>
+#include <xpdf/xpdf/Gfx.h>
+#include <xpdf/xpdf/GfxFont.h>
+#include <xpdf/xpdf/GfxState.h>
+#include <xpdf/xpdf/Link.h>
+#include <xpdf/xpdf/OutputDev.h>
+#include <xpdf/xpdf/Page.h>
+#include <xpdf/xpdf/Stream.h>
+#include <xpdf/xpdf/TextOutputDev.h>
+#include <xpdf/xpdf/TextString.h>
+#include <xpdf/xpdf/UnicodeMap.h>
+#include <xpdf/xpdf/XRef.h>
+
+#endif
--- include/xpdf/xpdf/config.h
+++ include/xpdf/xpdf/config.h
@@ -1,19 +1,23 @@
-//========================================================================
-//
-// config.h
-//
-// Copyright 1996-2022 Glyph & Cog, LLC
-//
-//========================================================================
+/*
+ *========================================================================
+ *
+ * config.h
+ *
+ * Copyright 1996-2022 Glyph & Cog, LLC
+ *
+ *========================================================================
+ */
 
 #ifndef XPDF_CONFIG_H
 #define XPDF_CONFIG_H
 
-//------------------------------------------------------------------------
-// version
-//------------------------------------------------------------------------
+/*
+ *------------------------------------------------------------------------
+ * version
+ *------------------------------------------------------------------------
+ */
 
-// xpdf version
+/* xpdf version */
 #define xpdfVersion          "4.04"
 #define xpdfVersionNum       4.04
 #define xpdfMajorVersion     4
@@ -23,50 +27,39 @@
 #define xpdfMinorVersionStr  "4"
 #define xpdfUpdateVersionStr "0"
 
-// supported PDF version
+/* supported PDF version */
 #define supportedPDFVersionStr "2.0"
 #define supportedPDFVersionNum 2.0
 
-// copyright notice
+/* copyright notice */
 #define xpdfCopyright "Copyright 1996-2022 Glyph & Cog, LLC"
 
-// Windows resource file stuff
+/* Windows resource file stuff */
 #define winxpdfVersion "WinXpdf 4.04"
 #define xpdfCopyrightAmp "Copyright 1996-2022 Glyph && Cog, LLC"
 
-//------------------------------------------------------------------------
-// paper size
-//------------------------------------------------------------------------
+/*
+ *------------------------------------------------------------------------
+ * paper size
+ *------------------------------------------------------------------------
+ */
 
-// default paper size (in points) for PostScript output
+/* default paper size (in points) for PostScript output */
 #ifdef XPDF_A4_PAPER
-#define defPaperWidth  595    // ISO A4 (210x297 mm)
+#define defPaperWidth  595    /* ISO A4 (210x297 mm) */
 #define defPaperHeight 842
 #else
-#define defPaperWidth  612    // American letter (8.5x11")
+#define defPaperWidth  612    /* American letter (8.5x11") */
 #define defPaperHeight 792
 #endif
 
-//------------------------------------------------------------------------
-// X-related constants
-//------------------------------------------------------------------------
+/*
+ *------------------------------------------------------------------------
+ * X-related constants
+ *------------------------------------------------------------------------
+ */
 
-// default maximum size of color cube to allocate
+/* default maximum size of color cube to allocate */
 #define defaultRGBCube 5
-
-//------------------------------------------------------------------------
-// popen
-//------------------------------------------------------------------------
-
-#if defined(_MSC_VER) || defined(__BORLANDC__)
-#define popen _popen
-#define pclose _pclose
-#endif
-
-#if defined(VMS) || defined(VMCMS) || defined(DOS) || defined(OS2) || defined(__EMX__) || defined(_WIN32) || defined(__DJGPP__)
-#define POPEN_READ_MODE "rb"
-#else
-#define POPEN_READ_MODE "r"
-#endif
 
 #endif
